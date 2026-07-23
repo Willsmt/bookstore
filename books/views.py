6da0e996fea8df85270ebe57547a8a15755259a6
Willsmt <willian_marthins@hotmail.com>
2026-07-23 08:13:53 -0300

    feat(viewsets): adiciona action reactivate para reverter soft delete

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -13,8 +15,19 @@ class BookViewSet(viewsets.ModelViewSet):
     ordering_fields = ["price", "stock", "published_at"]
 
     def get_queryset(self):
-        return Book.objects.filter(is_active=True)
+        qs = Book.objects.all()
+        if self.action == "reactivate":
+            return qs
+        return qs.filter(is_active=True)
 
     def perform_destroy(self, instance):
         instance.is_active = False
         instance.save(update_fields=["is_active"])
+
+    @action(detail=True, methods=["post"])
+    def reactivate(self, request, pk=None):
+        book = self.get_object()
+        book.is_active = True
+        book.save(update_fields=["is_active"])
+        serializer = self.get_serializer(book)
+        return Response(serializer.data)
--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 
 from product.models import Category, Product
 from product.serializers import CategorySerializer, ProductSerializer
@@ -19,8 +21,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     ordering_fields = ["price", "stock"]
 
     def get_queryset(self):
-        return Product.objects.filter(is_active=True).prefetch_related("categories")
+        qs = Product.objects.prefetch_related("categories")
+        if self.action == "reactivate":
+            return qs
+        return qs.filter(is_active=True)
 
     def perform_destroy(self, instance):
         instance.is_active = False
         instance.save(update_fields=["is_active"])
+
+    @action(detail=True, methods=["post"])
+    def reactivate(self, request, pk=None):
+        product = self.get_object()
+        product.is_active = True
+        product.save(update_fields=["is_active"])
+        serializer = self.get_serializer(product)
+        return Response(serializer.data)
