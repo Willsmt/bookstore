eec31cbabce447a7a16f07d78bd07d9f583c0ca6
Willsmt <willian_marthins@hotmail.com>
2026-07-23 09:07:29 -0300

    feat(category): adiciona reactivate e soft delete no CategoryViewSet

--- product/views.py
+++ product/views.py
@@ -7,10 +7,27 @@ from rest_framework.response import Response
 from product.models import Category, Product
 from product.serializers import CategorySerializer, ProductSerializer
 
-
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        qs = Category.objects.all()
+        if self.action == "reactivate":
+            return qs
+        return qs.filter(is_active=True)
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save(update_fields=["is_active"])
+
+    @action(detail=True, methods=["post"])
+    def reactivate(self, request, pk=None):
+        category = self.get_object()
+        category.is_active = True
+        category.save(update_fields=["is_active"])
+        serializer = self.get_serializer(category)
+        return Response(serializer.data)
+
 
 
 class ProductViewSet(viewsets.ModelViewSet):
