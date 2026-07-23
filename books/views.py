004d52724238f17723243b06f64b9f40bc162e3f
Willsmt <willian_marthins@hotmail.com>
2026-07-23 07:41:07 -0300

    feat(viewsets): otimiza queryset com prefetch_related e adiciona filtro, busca e ordenação

--- books/views.py
+++ books/views.py
@@ -1,11 +1,19 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
+
 from books.models import Book
 from books.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.filter(is_active=True)
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ["title", "author"]
+    ordering_fields = ["price", "stock", "published_at"]
+
+    def get_queryset(self):
+        return Book.objects.filter(is_active=True)
 
     def perform_destroy(self, instance):
         instance.is_active = False
--- product/views.py
+++ product/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
+
 from product.models import Category, Product
 from product.serializers import CategorySerializer, ProductSerializer
 
@@ -9,8 +12,14 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["categories"]
+    search_fields = ["title"]
+    ordering_fields = ["price", "stock"]
+
+    def get_queryset(self):
+        return Product.objects.filter(is_active=True).prefetch_related("categories")
 
     def perform_destroy(self, instance):
         instance.is_active = False
