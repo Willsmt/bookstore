e5feac5efe4b660b023bed1c858c7641ae430b88
Willsmt <willian_marthins@hotmail.com>
2026-07-25 02:59:15 -0300

    feat(orders): restringe OrderViewSet a TokenAuthentication

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly  
 from orders.models import Order
 from orders.serializers import OrderSerializer
 from core.pagination import OrderPagination
@@ -8,3 +10,5 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
